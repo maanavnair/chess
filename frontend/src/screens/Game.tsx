dba9e434ee550732f097371d40b3c15411e71426
Maanav Nair <maanavnair20042@gmail.com>
2024-10-30 18:00:56 +0530

    added chess.js to frontend

--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useState } from "react"
 import ChessBoard from "../components/ChessBoard"
 import { useSocket } from "../hooks/useSocket"
+import { Chess } from "chess.js"
 
 export const INIT_GAME = "init_game"
 export const MOVE = "move"
@@ -8,6 +10,31 @@ export const GAME_OVER = "game_over"
 const Game = () => {
 
     const socket = useSocket();
+    const [board, setBoard] = useState(new Chess());
+
+    useEffect(() => {
+        if (!socket) {
+            return;
+        }
+        socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            console.log(message);
+            switch (message.type) {
+                case INIT_GAME:
+                    setBoard(new Chess());
+                    console.log("Game initialised")
+                    break;
+                case MOVE:
+                    const move = message.payload;
+                    board.move(move);
+                    console.log("Move Made");
+                    break;
+                case GAME_OVER:
+                    console.log("Game Over");
+                    break;
+            }
+        }
+    }, [socket])
 
     if (!socket) return <div>Connecting...</div>
 
