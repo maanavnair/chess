95604d07b8fc5b0a9e47519dca83a09d456e7480
Maanav Nair <maanavnair20042@gmail.com>
2024-10-30 17:50:38 +0530

    socket custom hook:

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -2,7 +2,7 @@
 
 const ChessBoard = () => {
     return (
-        <div className="text-white">
+        <div className="text-white w-full">
             ChessBoard
         </div>
     )
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -1,15 +1,32 @@
 import ChessBoard from "../components/ChessBoard"
+import { useSocket } from "../hooks/useSocket"
+
+export const INIT_GAME = "init_game"
+export const MOVE = "move"
+export const GAME_OVER = "game_over"
 
 const Game = () => {
+
+    const socket = useSocket();
+
+    if (!socket) return <div>Connecting...</div>
+
     return (
         <div className="flex justify-center ">
-            <div className="pt-8 max-w-screen-lg">
-                <div className="grid grid-cols-6 gap-4 md:grid-cols-2">
+            <div className="pt-8 max-w-screen-lg w-full">
+                <div className="grid grid-cols-6 gap-4 w-full">
                     <div className="cols-span-4">
                         <ChessBoard />
                     </div>
                     <div className="cols-span-2">
-                        <button className="px-32 py-4 bg-green-500 hover:bg-green-700 text-white font-bold rounded">
+                        <button
+                            className="w-full px-8 py-4 bg-green-500 hover:bg-green-700 text-white font-bold rounded"
+                            onClick={() => {
+                                socket.send(JSON.stringify({
+                                    type: INIT_GAME
+                                }))
+                            }}
+                        >
                             Play
                         </button>
                     </div>
