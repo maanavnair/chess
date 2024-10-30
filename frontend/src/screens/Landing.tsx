7f89c7d49f58994df9f7b6c0994352f1d3dd6473
Maanav Nair <maanavnair20042@gmail.com>
2024-10-30 16:51:50 +0530

    done with landing page

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -1,9 +1,13 @@
+import { useNavigate } from "react-router-dom"
 
 
 const Landing = () => {
+
+    const navigate = useNavigate();
+
     return (
-        <div>
-            <div className="pt-2">
+        <div className="flex justify-center">
+            <div className="pt-8 max-w-screen-lg">
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                     <div className="flex justify-center">
                         <img src={"/chessboard.jpg"}
@@ -16,8 +20,11 @@ const Landing = () => {
                                 Play chess online on the #1 site!
                             </h1>
                         </div>
-                        <div className="mt-4 flex justify-center">
-                            <button className="p-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        <div className="mt-8 flex justify-center">
+                            <button
+                                className="px-8 py-4 bg-green-500 hover:bg-green-700 text-white font-bold rounded"
+                                onClick={() => navigate('/game')}
+                            >
                                 Play Online
                             </button>
                         </div>
