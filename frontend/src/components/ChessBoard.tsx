bc4ff9e359ef57630cca78967ba56d487985a528
Maanav Nair <maanavnair20042@gmail.com>
2024-11-01 19:12:52 +0530

    minor changes

--- frontend/src/components/ChessBoard.tsx
+++ frontend/src/components/ChessBoard.tsx
@@ -1,5 +1,6 @@
 import { Color, PieceSymbol, Square } from "chess.js"
 import { useState } from "react";
+import { MOVE } from "../screens/Game";
 
 
 const ChessBoard = ({ board, socket }: {
@@ -11,15 +12,33 @@ const ChessBoard = ({ board, socket }: {
     socket: WebSocket;
 }) => {
 
-    const [from, setFrom] = useState(null);
-    const [to, setTo] = useState(null);
+    const [from, setFrom] = useState<null | Square>(null);
+    const [to, setTo] = useState<null | Square>(null);
 
     return (
         <div className="text-white-200">
             {board.map((row, i) => {
                 return <div key={i} className="flex">
                     {row.map((square, j) => {
-                        return <div key={j} className={`text-black w-16 h-16 ${(i + j) % 2 == 0 ? 'bg-green-500' :
+                        return <div onClick={() => {
+                            if (!from) {
+                                setFrom(square?.square ? square.square : null);
+                            }
+                            else {
+                                socket.send(JSON.stringify({
+                                    type: MOVE,
+                                    payload: {
+                                        from,
+                                        to: square?.square
+                                    }
+                                }))
+                                setFrom(null);
+                                console.log({
+                                    from,
+                                    to
+                                })
+                            }
+                        }} key={j} className={`text-black w-16 h-16 ${(i + j) % 2 == 0 ? 'bg-green-500' :
                             'bg-white'}`}>
                             <div className="flex justify-center h-full w-full">
                                 <div className="h-full justify-center flex flex-col">
--- frontend/src/screens/Game.tsx
+++ frontend/src/screens/Game.tsx
@@ -46,7 +46,7 @@ const Game = () => {
             <div className="pt-8 max-w-screen-lg w-full">
                 <div className="grid grid-cols-6 gap-4 w-full">
                     <div className="col-span-4 w-full flex justify-center">
-                        <ChessBoard board={board} />
+                        <ChessBoard socket={socket} board={board} />
                     </div>
                     <div className="col-span-2 bg-slate-900 w-full flex justify-center">
                         <div className="pt-8">
