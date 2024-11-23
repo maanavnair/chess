a09007940b62b15f92d9220cfb3dc41efa742be6
Maanav Nair <maanavnair20042@gmail.com>
2024-11-23 18:08:18 +0530

    added server

--- backend1/dist/index.js
+++ backend1/dist/index.js
@@ -1,10 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const ws_1 = require("ws");
 const GameManager_1 = require("./GameManager");
-const wss = new ws_1.WebSocketServer({ port: 8080 });
+const express_1 = __importDefault(require("express"));
+const http_1 = __importDefault(require("http"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const server = http_1.default.createServer(app);
+const wss = new ws_1.WebSocketServer({ server });
 const gameManager = new GameManager_1.GameManager();
 wss.on('connection', function connection(ws) {
     gameManager.addUser(ws);
     ws.on("disconnect", () => gameManager.removeUser(ws));
+});
+server.listen(8080, () => {
+    console.log("Server Started");
 });
--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,7 +1,14 @@
 import { WebSocketServer } from 'ws';
 import { GameManager } from './GameManager';
+import express from "express";
+import http from "http";
 
-const wss = new WebSocketServer({ port: 8080 });
+const app = express();
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server });
 
 const gameManager = new GameManager();
 
@@ -9,3 +16,7 @@ wss.on('connection', function connection(ws) {
     gameManager.addUser(ws);
     ws.on("disconnect", () => gameManager.removeUser(ws));
 });
+
+server.listen(8080, () => {
+    console.log("Server Started");
+})
